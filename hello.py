53b7302b35ea2d6b9f38b938a21e76abd11d4a80
Ikboljon Kakhkhorov <simon.lily1322@gmail.com>
2020-01-31 20:48:54 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+#clear the terminal screen
+import os 
+os.system('clear')
+
+
+first_name = 'Iqboljon'
+age = 41 
+fav_pizza = {
+	"Iqboljon": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"
+ }
+ name = True 
+
+ print (name)
+ 
